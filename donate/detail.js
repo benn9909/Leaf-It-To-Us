2ee94c6e99299083160ff770ea86112113929720
benn9909 <duy9909@gmail.com>
2025-03-16 08:46:13 +0700

    a

--- donate/detail.js
+++ donate/detail.js
@@ -6,7 +6,7 @@ let app = document.getElementById('app');
 let temporaryContent = document.getElementById('temporaryContent');
 
 const loadTemplate = () => {
-    fetch('/template.html')
+    fetch('template.html')
     .then(response => response.text())
     .then(html => {
         app.innerHTML = html;
@@ -38,7 +38,7 @@ const initApp = () => {
         let newProduct = document.createElement('div');
         newProduct.classList.add('item');
         newProduct.innerHTML = 
-        `<a href="/detail.html?id=${product.id}">
+        `<a href="detail.html?id=${product.id}">
             <img src="${product.image}">
         </a>
         <h2>${product.name}</h2>
--- donate/index.js
+++ donate/index.js
@@ -6,7 +6,7 @@ let temporaryContent = document.getElementById('temporaryContent');
 
 // load layout file
 const loadTemplate = () => {
-    fetch('/template.html')
+    fetch('template.html')
     .then(response => response.text())
     .then(html => {
         app.innerHTML = html;
@@ -27,7 +27,7 @@ const initApp = () => {
          let newProduct = document.createElement('div');
          newProduct.classList.add('item');
          newProduct.innerHTML = 
-         `<a href="/detail.html?id=${product.id}">
+         `<a href="detail.html?id=${product.id}">
              <img src="${product.image}">
          </a>
          <h2>${product.name}</h2>
